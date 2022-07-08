73d47824505076a75066ff1a98f499dada90b75c
modavid@mail.ru <modavid@mail.ru>
2022-07-08 20:32:58 +0400

    homework1

---
+++ lesson1dz1/lesson1dz1.py
@@ -0,0 +1,4 @@
+name = "David"
+age = 27
+a = 27 - 18
+print(name, "на", a, "лет больше '18'")
---
+++ lesson1dz1/lesson1dz3.py
@@ -0,0 +1,15 @@
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+c = int(input('Enter 3nd number: '))
+d = b ** 2 - 4 * a * c
+import math
+
+if d < 0:
+    print("not roots")
+elif d == 0:
+    x = - b / 2 * a
+else:
+    x1 = (-b + math.sqrt(d)) / (2 * a)
+    x2 = (-b - math.sqrt(d)) / (2 * a)
+    print(x1)
+    print(x2)
